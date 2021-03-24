d8b0a540e1990708e4e86108e81e82ae43a78c50
Miek Gieben <miek@miek.nl>
2021-03-24 14:56:13 +0100

    add
    Signed-off-by: Miek Gieben <miek@miek.nl>

---
+++ handler.go
@@ -0,0 +1,16 @@
+package lboverlay
+
+import (
+	"context"
+
+	"github.com/miekg/dns"
+)
+
+// ServeDNS implements the plugin.Plugin interface
+func (o *Overlay) ServeDNS(ctx context.Context, w dns.ResponseWriter, r *dns.Msg) (int, error) {
+	// check domains we're repsonbile for
+	// check if HC query
+	// check if normal query
+
+	return 0, nil
+}
