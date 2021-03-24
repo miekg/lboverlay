26da7624c0896cb6b1578c158437d19a15968c0a
Miek Gieben <miek@miek.nl>
2021-03-24 17:44:41 +0100

    docs
    Signed-off-by: Miek Gieben <miek@miek.nl>

--- handler.go
+++ handler.go
@@ -9,8 +9,9 @@ import (
 // ServeDNS implements the plugin.Plugin interface.
 func (o *Overlay) ServeDNS(ctx context.Context, w dns.ResponseWriter, r *dns.Msg) (int, error) {
 	// check domains we're repsonbile for
-	// check if HC query
-	// check if normal query
+	// check if HC query and update cache (should we discard, or just go with last know good)
+	// each should be updated every 10s, so older entries >1h could be removed.
+	// check if normal query and do what is described in the README
 
 	return 0, nil
 }
